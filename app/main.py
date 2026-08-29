af55ae89724f461eb44970e0f9818567b6a129ed
nif <nif104x@gmail.com>
2026-08-30 00:26:27 +0600

    middleware setup

--- app/main.py
+++ app/main.py
@@ -4,8 +4,19 @@ from fastapi.staticfiles import StaticFiles
 
 from app.routes import admin, booking, conversation, listings, notification, report, review, search, user, wishlist, login
 from app.db import engine
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
 
 app.include_router(admin.router)
 app.include_router(booking.router)
