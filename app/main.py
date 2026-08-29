96d64a2d66d4dda9815b8886326b7586e33aed50
nif <nif104x@gmail.com>
2026-08-30 00:28:01 +0600

    middleware setup

--- app/main.py
+++ app/main.py
@@ -7,14 +7,17 @@ from app.db import engine
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://localhost:5173",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 
 
